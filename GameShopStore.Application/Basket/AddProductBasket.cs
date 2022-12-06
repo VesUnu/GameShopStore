b08a0dee6c490135ce07baedc2b92f279175080b
Veselin Uzuntonev <vesi_valq@abv.bg>
2022-12-06 11:31:05 +0200

    Added another Interface.

--- GameShopStore.Application/Basket/AddProductBasket.cs
+++ GameShopStore.Application/Basket/AddProductBasket.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace GameShopStore.Application.Basket
 {
-    public class AddProductBasket
+    public class AddProductBasket : IAdd
     {
 
     }
---
+++ GameShopStore.Application/Interfaces/IAddStockBasket.cs
@@ -0,0 +1,15 @@
+﻿using GameShopStore.Core.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameShopStore.Application.Interfaces
+{
+    public interface IAddStockBasket
+    {
+        void Do(ISession session, StockOnHold stockToUpdate);
+    }
+}
