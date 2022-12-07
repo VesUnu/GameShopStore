205c8d0605c27533414d192704ba5212a58263a0
Veselin Uzuntonev <vesi_valq@abv.bg>
2022-12-07 18:17:49 +0200

    Starting to work on the Infrastructure folder.

--- GameShopStore.Infrastructure/ApplicationDbContext.cs
+++ GameShopStore.Infrastructure/ApplicationDbContext.cs
@@ -1,13 +1,21 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using GameShopStore.Application.Helpers;
+using Microsoft.Extensions.Configuration;
+using GameShopStore.Core.Models;
 
 namespace GameShopStore.Infrastructure
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<User, Role, int, IdentityUserClaim<int>,
+        UserRole, IdentityUserLogin<int>, IdentityRoleClaim<int>, IdentityUserToken<int>>
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        private readonly IConfiguration _seedDataOptions;
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IConfiguration seedDataOptions)
             : base(options)
         {
+            _seedDataOptions = seedDataOptions;
         }
     }
 }
---
+++ GameShopStore.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using GameShopStore.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameShopStore.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _appDbCtx;
+
+        public UnitOfWork(ApplicationDbContext appDbCtx)
+        {
+            _appDbCtx = appDbCtx;
+        }
+
+        private IRequirementsRepo _requirements;
+        public IRequirementsRepo Requirements
+        {
+            get
+            {
+                if (_requirements = null)
+                {
+                    _requirements = new RequirementsRepo(_appDbCtx)
+                }
+
+                return _requirements;
+            }
+        }
+    }
+}
