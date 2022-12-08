e2832c3a7fc9993ae0d0eab04d132ade958f861c
Veselin Uzuntonev <vesi_valq@abv.bg>
2022-12-08 12:07:28 +0200

    Added an interface for the Identity.

---
+++ GameShopStore.Infrastructure/Interfaces/IJwtTokenHelper.cs
@@ -0,0 +1,16 @@
+﻿using GameShopStore.Core.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameShopStore.Infrastructure.Interfaces
+{
+    public interface IJwtTokenHelper
+    {
+        Task<string> GenerateJwtToken(User user, UserManager<User> userManager, IConfiguration config);
+    }
+}
