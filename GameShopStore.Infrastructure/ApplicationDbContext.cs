176af0dd26bd4f8600bb241690d8c0bf8c69ec19
Veselin Uzuntonev <vesi_valq@abv.bg>
2022-12-09 12:54:37 +0200

    Finish up the ApplicationDbContext.

--- GameShopStore.Infrastructure/ApplicationDbContext.cs
+++ GameShopStore.Infrastructure/ApplicationDbContext.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using GameShopStore.Application.Helpers;
 using Microsoft.Extensions.Configuration;
 using GameShopStore.Core.Models;
+using GameShopStore.Infrastructure.Extensions;
+using GameShopStore.Infrastructure.Configs;
 
 namespace GameShopStore.Infrastructure
 {
@@ -33,5 +35,102 @@ namespace GameShopStore.Infrastructure
         public DbSet<StockOnHold> StockOnHolds { get; set; }
         public DbSet<Address> Addresses { get; set; }
         public DbSet<DeliveryOpt> DeliveryOpts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+
+            builder.SeedProducts(_seedDataOptions);
+
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfiguration(new ProductConfig());
+            builder.ApplyConfiguration(new RequirementsConfig());
+            builder.ApplyConfiguration(new LanguageConfig());
+            builder.ApplyConfiguration(new PictureConfig());
+            builder.ApplyConfiguration(new CategoryConfig());
+            builder.ApplyConfiguration(new SubCategoryConfig());
+            builder.ApplyConfiguration(new OrderConfig());
+            builder.ApplyConfiguration(new OrderStockConfig());
+            builder.ApplyConfiguration(new StockOnHoldConfig());
+            builder.ApplyConfiguration(new UserConfig());
+            builder.ApplyConfiguration(new AddressConfig());
+            builder.ApplyConfiguration(new DeliveryOptConfig());
+
+
+            builder.Entity<Product>()
+                .HasOne(p => p.Requirements)
+                .WithOne(r => r.Product)
+                .HasForeignKey<Requirements>(r => r.ProductId);
+
+            builder.Entity<CategorySubCategory>()
+                .HasKey(k => new { k.CategoryId, k.SubCategoryId });
+
+            builder.Entity<CategorySubCategory>()
+                .HasOne(csc => csc.Category)
+                .WithMany(c => c.SubCategories)
+                .HasForeignKey(csc => csc.CategoryId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<CategorySubCategory>()
+                .HasOne(csc => csc.SubCategory)
+                .WithMany(sc => sc.Categories)
+                .HasForeignKey(csc => csc.SubCategoryId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+
+
+            builder.Entity<ProductSubCategory>()
+                .HasKey(k => new { k.ProductId, k.SubCategoryId });
+
+            builder.Entity<ProductSubCategory>()
+                .HasOne(psc => psc.Product)
+                .WithMany(p => p.SubCategories)
+                .HasForeignKey(psc => psc.ProductId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<ProductSubCategory>()
+                .HasOne(psc => psc.SubCategory)
+                .WithMany(sc => sc.Products)
+                .HasForeignKey(psc => psc.SubCategoryId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+
+            builder.Entity<ProductLanguage>()
+                .HasKey(k => new { k.ProductId, k.LanguageId });
+
+            builder.Entity<ProductLanguage>()
+                .HasOne(pl => pl.Product)
+                .WithMany(p => p.Languages)
+                .HasForeignKey(pl => pl.ProductId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<ProductLanguage>()
+                .HasOne(pl => pl.Language)
+                .WithMany(l => l.Products)
+                .HasForeignKey(pl => pl.LanguageId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+
+
+            builder.Entity<UserRole>(userRole =>
+            {
+                userRole.HasKey(ur => new { ur.UserId, ur.RoleId });
+
+                userRole.HasOne(ur => ur.Role)
+                    .WithMany(r => r.UserRoles)
+                    .HasForeignKey(ur => ur.RoleId)
+                    .IsRequired();
+
+                userRole.HasOne(ur => ur.User)
+                .WithMany(u => u.UserRoles)
+                .HasForeignKey(ur => ur.UserId)
+                .IsRequired();
+            });
+
+            builder.Entity<OrderStock>()
+                .HasKey(k => new { k.StockId, k.OrderId });
+        }
+
     }
 }
